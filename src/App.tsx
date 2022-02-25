b24d9b4c88b12e8903d823a75b1ed5222b4e9a1d
yeonlisa <tkzw4869@gmail.com>
2022-02-25 17:33:57 +0900

    useRef Hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import './App.css';
-import { UserContextProvider } from './components/context/UserContext';
-import { User } from './components/context/User';
 
 function App() {
   return (
     <div className="App">
-      <UserContextProvider>
-        <User />
-      </UserContextProvider>
+
     </div>
   );
 }
