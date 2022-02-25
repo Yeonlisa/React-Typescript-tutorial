43a83e979d49c426315862723d17a4e221ef49e3
yeonlisa <tkzw4869@gmail.com>
2022-02-25 17:16:40 +0900

    useContext Hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import { ThemeContextProvider } from './components/context/ThemeContext';
+import { Box } from './components/context/Box';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <ThemeContextProvider>
+        <Box />
+      </ThemeContextProvider>
     </div>
   );
 }
