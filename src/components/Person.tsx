c40fac9a45d37a5d00b89901e26136fce897bacd
yeonlisa <tkzw4869@gmail.com>
2022-02-25 15:13:33 +0900

    Prop Types and Tips

--- src/components/Person.tsx
+++ src/components/Person.tsx
@@ -1,9 +1,4 @@
-type PersonProps = {
-    name: {
-        first: String
-        last: string
-    }
-}
+import { PersonProps } from "./Person.types"
 
 export const Person = (props: PersonProps) => {
     return (
