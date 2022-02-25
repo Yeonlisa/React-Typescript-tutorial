502c8dfa02c5a1745b12079f9240bd65cdc18b39
yeonlisa <tkzw4869@gmail.com>
2022-02-25 15:05:08 +0900

    Style Props

---
+++ src/components/Container.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+type ContainerProps = {
+    styles: React.CSSProperties
+}
+
+export const Container = (props: ContainerProps) => {
+    return (
+        <div style={props.styles}>
+            Text content goes here
+        </div>
+    )
+}
